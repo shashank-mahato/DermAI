54704a95ce81b125cd6fac185f5c34a6d5e9f9e0
Shashank Mahato <99711397+shashank-mahato@users.noreply.github.com>
2024-11-08 21:24:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -120,4 +120,4 @@ if __name__ == '__main__':
     if not os.path.exists(UPLOAD_FOLDER):
         os.makedirs(UPLOAD_FOLDER)
 
-    app.run(debug=False, host="0.0.0.0", port=5000)
+    app.run(debug=False, host="0.0.0.0", port=10000)
